9ffecaf329b4cd7f24ef9cd3852d32cdea797396
Prabhanshu <prabhanshu.itworks@gmail.com>
2022-04-11 19:22:41 +0530

    User model updated with table details

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,28 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  sign_in_count          :integer          default(0), not null
+#  provider               :string
+#  uid                    :string
+#  name                   :string
+#  image                  :text
+#  disableyn              :string          
+#  admin                  :string
+#  phone                  :string
+#  age                    :string
+#  gender                 :string
+#  address                :string
+
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
